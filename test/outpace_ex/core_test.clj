75620f67bdfb4ae3130d0b565c19f7d4957d7376
Alex Stoddard <alexander.stoddard@gmail.com>
2014-12-04 23:20:50 -0600

    Test examples for valid-checksum? added.

--- test/outpace_ex/core_test.clj
+++ test/outpace_ex/core_test.clj
@@ -8,3 +8,9 @@
   (testing "parse-digits on all ocr examples"
     (doseq [{:keys [input result]} story1-string-data]
       (is (= (parse-digits input) result)))))
+
+
+(deftest valid-checksum
+  (testing "A valid account number"
+    (is (valid-checksum? [4 5 7 5 0 8 0 0 0]) "Should be valid")
+    (is (not (valid-checksum? [6 6 4 3 7 1 4 9 5])) "Should be invalid")))
