2772a8ca1ebbd35098f8d2c56419a1979c0f9125
Alex Stoddard <alexander.stoddard@gmail.com>
2014-12-08 21:48:05 -0600

    Implemented file validation tests.

--- test/outpace_ex/core_test.clj
+++ test/outpace_ex/core_test.clj
@@ -32,3 +32,25 @@
         (is (valid-checksum? good-acc) "Should be valid"))
       (doseq [bad-acc invalid-accounts]
         (is (not (valid-checksum? bad-acc)) "Should be invalid")))))
+
+(let 
+    [res-file (fn [f] (-> f 
+                          (clojure.java.io/resource)
+                          (clojure.java.io/file)))
+     good-file (res-file "valid_data.txt")
+     bad-rec-size-file (res-file "invalid_data_recsize.txt")
+     bad-line-ln-file (res-file "invalid_data_line_ln.txt")
+     bad-char-in-file (res-file "invalid_data_bad_char.txt")]
+  (deftest valid-data
+    (let [good-records (extract-records (read-input-data good-file))]
+      (is (validate-records good-records)))
+    (let [bad-rec-size (extract-records (read-input-data bad-rec-size-file))]
+      (is (thrown-with-msg? java.lang.AssertionError #"not three lines long"
+                   (validate-records bad-rec-size))))
+    (let [bad-char-rec (extract-records (read-input-data bad-char-in-file))]
+      (is (thrown-with-msg? java.lang.AssertionError #"has other than | _ or \\space"
+                   (validate-records bad-char-rec))))
+    (let [bad-line-length (extract-records (read-input-data bad-line-ln-file))]
+      (is (thrown-with-msg? java.lang.AssertionError #"not 27 chars long"
+                   (validate-records bad-line-length))))))
+  
